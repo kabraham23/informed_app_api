1a35897dc899a4630b3500f7906dcbd8f61f71ae
Katie <katie@katies-air.lan>
2020-08-07 20:49:23 -0400

    help

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #   allow do
-#     origins 'example.com'
-#
+#     origins 'https://5f2da65c284ce500075d4ea6--eloquent-brahmagupta-65a863.netlify.app/'
+
 #     resource '*',
 #       headers: :any,
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
